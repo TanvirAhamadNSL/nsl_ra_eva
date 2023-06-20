db7d233b2c81d7929419ec2074ea6d4aa38caac1
tanvir_nsl <tanvir@nextsolutionlab.com>
2023-06-20 11:10:04 +0600

    [fix] fix B2.py file

--- stage_1/B/B2.py
+++ stage_1/B/B2.py
@@ -1,1 +1,13 @@
+def iterator(start, end):
+    print("*********", start, end)
+    pass
 
+
+print("=====1=====")
+iterator(10, 10)
+print("=====2=====")
+# iterator(start=10, 20)
+print("=====3=====")
+iterator(10, end=10)
+print("=====4=====")
+iterator(start=10, end=20)
